9ed243c9412c277c4d45645ca3fe3fb5d4b620ce
Sebas Túnchez <132542854+Tunchxz@users.noreply.github.com>
2023-08-14 01:34:17 -0600

    Métodos no "Set" o "Get"
    Implementación de métodos como:
    - Solicitar Compra
    - Asignar Localidad
    - Solicitud Boletos

--- DriverProgram.java
+++ DriverProgram.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class DriverProgram {
 
     public static void main(String[] args) {
@@ -55,20 +58,122 @@ class Comprador {
     }
 
     public boolean solicitarCompra() {
+        Random numero = new Random();
+        ticket = numero.nextInt(15000) + 1;
+        int A = numero.nextInt(15000) + 1;
+        int B = numero.nextInt(15000) + 1;
+        int rangoMin = Math.min(A, B);
+        int rangoMax = Math.max(A, B);
+
+        return ticket >= rangoMin && ticket <= rangoMax;
     }
 
     public int asignarLocalidad() {
+        Random numero = new Random();
+        int localidad = numero.nextInt(3);
+        if (localidad == 0) {
+            return 1;
+        } else if (localidad == 1) {
+            return 5;
+        } else {
+            return 10;
+        }
     }
 }
 
 class Localidad {
     private Comprador swiftie;
 
-    public Localidad() {
+    public Localidad(Comprador swiftie) {
+        this.swiftie = swiftie;
     }
 
-    public void solicitudColetos(Comprador swiftie, Validador validador) {
-        this.swiftie = swiftie;
+    public void solicitudBoletos(Validador validador) {
+        int localidad = swiftie.asignarLocalidad();
+        int boletosDeseados = swiftie.getCantidadBoletos();
+        int presupuesto = swiftie.getPresupuestoMax();
+
+        switch (localidad) {
+            case 1: {
+                int boletosLocal1 = validador.getLocalidad1();
+                // Validar espacio
+                if (boletosLocal1 <= 0) {
+                    System.out.println("Lo sentimos, los boletos para Localidad 1 están agotados.");
+                    break;
+                }
+                // Validar disponibilidad de boletos deseados
+                int boletosAVender = Math.min(boletosDeseados, boletosLocal1);
+
+                // c. Validar precio
+                int boletosPosibles = presupuesto / 100;
+                boletosAVender = Math.min(boletosAVender, boletosPosibles);
+
+                if (boletosAVender != 0) {
+                    String totalPrecio = "" + (boletosAVender * 100);
+                    String totalBoletos = "" + boletosAVender;
+                    System.out.println("----- ¡FELICIDADES! COMPRA EXITOSA -----\nPreio total: Q." + totalPrecio
+                            + ".00\nLocalidad: 1\nEntradas Totales: " + totalBoletos);
+                    validador.setLocalidad1(validador.getLocalidad1() - boletosAVender);
+                    break;
+                }
+                System.out.println("Lo sentimos, tu presupuesto es insuficiente para un boleto.");
+                break;
+            }
+            case 5: {
+                int boletosLocal5 = validador.getLocalidad5();
+                // Validar espacio
+                if (boletosLocal5 <= 0) {
+                    System.out.println("Lo sentimos, los boletos para Localidad 5 están agotados.");
+                    break;
+                }
+                // Validar disponibilidad de boletos deseados
+                int boletosAVender = Math.min(boletosDeseados, boletosLocal5);
+
+                // c. Validar precio
+                int boletosPosibles = presupuesto / 500;
+                boletosAVender = Math.min(boletosAVender, boletosPosibles);
+
+                if (boletosAVender != 0) {
+                    String totalPrecio = "" + (boletosAVender * 500);
+                    String totalBoletos = "" + boletosAVender;
+                    System.out.println("----- ¡FELICIDADES! COMPRA EXITOSA -----\nPreio total: Q." + totalPrecio
+                            + ".00\nLocalidad: 5\nEntradas Totales: " + totalBoletos);
+                    validador.setLocalidad5(validador.getLocalidad5() - boletosAVender);
+                    break;
+                }
+                System.out.println("Lo sentimos, tu presupuesto es insuficiente para un boleto.");
+                break;
+            }
+            case 10: {
+                int boletosLocal10 = validador.getLocalidad10();
+                // Validar espacio
+                if (boletosLocal10 <= 0) {
+                    System.out.println("Lo sentimos, los boletos para Localidad 10 están agotados.");
+                    break;
+                }
+                // Validar disponibilidad de boletos deseados
+                int boletosAVender = Math.min(boletosDeseados, boletosLocal10);
+
+                // c. Validar precio
+                int boletosPosibles = presupuesto / 1000;
+                boletosAVender = Math.min(boletosAVender, boletosPosibles);
+
+                if (boletosAVender != 0) {
+                    String totalPrecio = "" + (boletosAVender * 1000);
+                    String totalBoletos = "" + boletosAVender;
+                    System.out.println("----- ¡FELICIDADES! COMPRA EXITOSA -----\nPreio total: Q." + totalPrecio
+                            + ".00\nLocalidad: 10\nEntradas Totales: " + totalBoletos);
+                    validador.setLocalidad10(validador.getLocalidad10() - boletosAVender);
+                    break;
+                }
+                System.out.println("Lo sentimos, tu presupuesto es insuficiente para un boleto.");
+                break;
+            }
+            default: {
+                System.out.println("No hemos podido encontrar tu solicitud");
+                break;
+            }
+        }
     }
 }
 
